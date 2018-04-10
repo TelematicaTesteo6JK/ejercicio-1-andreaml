2e4f116adbb7176e6392214557d7aa0a4528a302
Andrea Muñoz <andreamunozliy@gmail.com>
2018-04-10 00:38:52 -0500

    Se incluye librería string y se modifica impresión de resultados.

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 /*
@@ -20,7 +21,7 @@ float pago_estacionamiento(int horas)
 	return cant;
 }
 
-String pruebaFuncion(float valorEsperado, float valorReal) {
+string pruebaFuncion(float valorEsperado, float valorReal) {
 	if (valorEsperado == valorReal) {
 		return "Pass";
 	} else {
@@ -34,20 +35,21 @@ void casos_de_prueba(){
     // Agrega tus casos de prueba
 	cout << "--> Equivalence Partitioning <--" << endl;
 	cout << "Test data\tExpected result\tActual result" <<  endl;
-	cout << -2 	<< "\t" << 0 	<< pago_estacionamiento(-2) << "\t" + pruebaFuncion(0.0, pago_estacionamiento(-2))	<< endl;
-	cout << 3 	<< "\t" << 30	<< pago_estacionamiento(3) 	<< "\t" + pruebaFuncion(30, pago_estacionamiento(3))	<< endl;
-	cout << 8 	<< "\t" << 72	<< pago_estacionamiento(8) 	<< "\t" + pruebaFuncion(72, pago_estacionamiento(8))	<< endl;
-	cout << 22 	<< "\t" << 154	<< pago_estacionamiento(22) << "\t" + pruebaFuncion(154, pago_estacionamiento(22))	<< endl << endl;
+	cout << -2 	<< "\t\t" << "Error"<< "\t\t" <<	pago_estacionamiento(-2)	<< "\t\t" + pruebaFuncion(-1, pago_estacionamiento(-2))<< endl;
+	cout << 3 	<< "\t\t" << 30		<< "\t\t" <<	pago_estacionamiento(3) 	<< "\t\t" + pruebaFuncion(30, pago_estacionamiento(3))	<< endl;
+	cout << 8 	<< "\t\t" << 72		<< "\t\t" <<	pago_estacionamiento(8) 	<< "\t\t" + pruebaFuncion(72, pago_estacionamiento(8))	<< endl;
+	cout << 22 	<< "\t\t" << 154	<< "\t\t" <<	pago_estacionamiento(22) 	<< "\t\t" + pruebaFuncion(154, pago_estacionamiento(22))<< endl << endl;
 	
 	cout << "--> Equivalence Partitioning <--" << endl;
-	cout << 0 	<< "\t" << 0	<< pago_estacionamiento(0) 	<< "\t" + pruebaFuncion(0, pago_estacionamiento(0))		<< endl;
-	cout << 1 	<< "\t" << 10	<< pago_estacionamiento(1) 	<< "\t" + pruebaFuncion(10, pago_estacionamiento(1))	<< endl;
-	cout << 5 	<< "\t" << 50	<< pago_estacionamiento(5) 	<< "\t" + pruebaFuncion(50, pago_estacionamiento(5))	<< endl;
-	cout << 6 	<< "\t" << 54	<< pago_estacionamiento(6) 	<< "\t" + pruebaFuncion(54, pago_estacionamiento(6))	<< endl;
-	cout << 10 	<< "\t" << 90	<< pago_estacionamiento(10) << "\t" + pruebaFuncion(90, pago_estacionamiento(10))	<< endl;
-	cout << 11 	<< "\t" << 77	<< pago_estacionamiento(11) << "\t" + pruebaFuncion(77, pago_estacionamiento(11))	<< endl;
-	cout << 23 	<< "\t" << 161	<< pago_estacionamiento(23) << "\t" + pruebaFuncion(161, pago_estacionamiento(23))	<< endl;
-	cout << 25 	<< "\t" << 175	<< pago_estacionamiento(25) << "\t" + pruebaFuncion(175, pago_estacionamiento(25))	<< endl;
+	cout << "Test data\tExpected result\tActual result" <<  endl;
+	cout << 0 	<< "\t\t" << "Error"<< "\t\t" <<	pago_estacionamiento(0)		<< "\t\t" + pruebaFuncion(-1, pago_estacionamiento(0))		<< endl;
+	cout << 1 	<< "\t\t" << 10		<< "\t\t" <<	pago_estacionamiento(1) 	<< "\t\t" + pruebaFuncion(10, pago_estacionamiento(1))		<< endl;
+	cout << 5 	<< "\t\t" << 50		<< "\t\t" <<	pago_estacionamiento(5) 	<< "\t\t" + pruebaFuncion(50, pago_estacionamiento(5))		<< endl;
+	cout << 6 	<< "\t\t" << 54		<< "\t\t" <<	pago_estacionamiento(6) 	<< "\t\t" + pruebaFuncion(54, pago_estacionamiento(6))		<< endl;
+	cout << 10 	<< "\t\t" << 90		<< "\t\t" <<	pago_estacionamiento(10)	<< "\t\t" + pruebaFuncion(90, pago_estacionamiento(10))		<< endl;
+	cout << 11 	<< "\t\t" << 77		<< "\t\t" <<	pago_estacionamiento(11) 	<< "\t\t" + pruebaFuncion(77, pago_estacionamiento(11))		<< endl;
+	cout << 23 	<< "\t\t" << 161	<< "\t\t" <<	pago_estacionamiento(23) 	<< "\t\t" + pruebaFuncion(161, pago_estacionamiento(23))	<< endl;
+	cout << 25 	<< "\t\t" << "Error"<< "\t\t" <<	pago_estacionamiento(25) 	<< "\t\t" + pruebaFuncion(-1, pago_estacionamiento(25))		<< endl;
 
     cout << endl;
 
@@ -55,8 +57,6 @@ void casos_de_prueba(){
 
 int main()
 {
-
     casos_de_prueba();
-
 	return 0;
 }
